e8033a68349f74f1c866df23d18fdcdb7bdaf355
Apolônio Serafim <apolonio.serafim@hotmail.com>
2020-06-17 15:58:19 -0300

    channel info

---
+++ src/components/ChannelInfo/index.tsx
@@ -0,0 +1,22 @@
+﻿import React from 'react';
+
+import { Container, HashTagIcon, Title, Separator, Description } from './styles';
+
+import ServerList from '../ServerList';
+import ServerName from '../ServerName';
+
+const ChannelInfo: React.FC = () => {
+  return (
+    <Container>
+      <HashTagIcon />
+
+      <Title>chat-livre</Title>
+
+      <Separator />
+
+      <Description>Canal aberto para conversas</Description>
+    </Container>
+  )
+};
+
+export default ChannelInfo;
