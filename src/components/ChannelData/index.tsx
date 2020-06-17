5730bf4d7a04e90de08fa08406e3643b9d73f5b3
Apolônio Serafim <apolonio.serafim@hotmail.com>
2020-06-17 18:17:59 -0300

    edit channel data

---
+++ src/components/ChannelData/index.tsx
@@ -0,0 +1,26 @@
+﻿import React from 'react';
+
+import ChannelMessage from '../ChannelMessage';
+
+import { Container, Messages, InputWrapper, Input, IpuntIcon } from './styles';
+
+const ChannelData: React.FC = () => {
+  return (
+    <Container>
+      <Messages>
+        <ChannelMessage
+          author="Apolônio Serafim"
+          date="17/06/2020"
+          content="Hoje é um grande dia"
+        />
+      </Messages>
+
+      <InputWrapper>
+        <Input type="text" placeholder="Conversar em #chat-livre" />
+        <IpuntIcon />
+      </InputWrapper>
+    </Container>
+  )
+};
+
+export default ChannelData;
